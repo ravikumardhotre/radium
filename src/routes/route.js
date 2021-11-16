e60a46f5071a805a7d600414a2b702b1bf1f8471
ravikumardhotre <dhotreravi17@gmail.com>
2021-11-16 10:47:19 +0530

    assignment on schema-basic2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,24 @@
 const express = require('express');
-
 const router = express.Router();
+const UserModel = require("../models/userModel")
 
-router.get('/test-me', function (req, res) {
+const UserController = require("../controllers/userController")
+const BookController = require("../controllers/bookController")
+
+
+router.get('/test-me', function(req, res) {
     res.send('My first ever api!')
 });
+
+// this are the api
+
+router.post('/createBook', BookController.createBook);
+router.get('/getAllBooks', BookController.getBooksData);
+
+router.post('/getBooksInYear', BookController.getBooksInYear)
+router.post('/getParticularBooks', BookController.getParticularBooks)
+
+router.get('/getXINRBooks', BookController.getXINRBooks)
+router.get('/getRandomBooks', BookController.getRandomBooks)
 
 module.exports = router;
