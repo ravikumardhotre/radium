2fc9cd09de4b95c6c6d668ce217c75a1ff1eaae4
Ravikumar Dhotre <93571776+ravikumardhotre@users.noreply.github.com>
2021-12-24 09:11:59 +0530

    update

--- src/middleware/loginmiddle.js
+++ src/middleware/loginmiddle.js
@@ -2,22 +2,19 @@ const jwt = require('jsonwebtoken')
 
 const userAuth = async(req, res, next) => {
     try {
-        const token = req.Authorization('Bearer Token')
+        const token = req.headers['authorization']
+        console.log("token is generated-->", token)
         if (!token) {
             res.status(403).send({ status: false, message: `Missing authentication token in request` })
-            return;
+        } else {
+            const decoded = await jwt.verify(token && token.split(' ')[1], 'group5')
+            if (decoded) {
+                console.log(decoded)
+                req.userId = decoded.userId
+                console.log(req.userId)
+                next()
+            }
         }
-
-        const decoded = await jwt.verify(token, 'soramoki')
-
-        if (!decoded) {
-            res.status(403).send({ status: false, message: `Invalid authentication token in request` })
-            return;
-        }
-
-        req.userId = decoded.userId;
-
-        next()
     } catch (error) {
         console.error(`Error! ${error.message}`)
         res.status(500).send({ status: false, message: error.message })
