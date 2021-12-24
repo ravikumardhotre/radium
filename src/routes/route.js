04bfd4a69f4498eb52b59c121545b489da60514a
Ravikumar Dhotre <93571776+ravikumardhotre@users.noreply.github.com>
2021-12-24 09:11:02 +0530

    update route

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
-const loginmiddle = require('../middleware/loginmiddle')
+const middleware1 = require('../middleware/loginmiddle')
 
 router.post('/register', userController.userCreation)
 router.post('/login', userController.userLogin)
-    //router.get('/user/:userId/profile', loginmiddle.userAuth, userController.updateProfile)
-    //router.put('/user/:userId/profile')
+router.get('/user/:userId/profile', middleware1, userController.getUserDetails)
+router.put('/user/:userId/profile', middleware1, userController.updateUser)
 
 module.exports = router;
